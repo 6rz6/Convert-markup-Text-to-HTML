6a3ffdb0e8497d15e61d12eea58c713c30e94204
6rz6 <102882394+6rz6@users.noreply.github.com>
2023-05-31 21:17:41 +0700

    renamed Convert_MarkupText_to_HTML.py

---
+++ Convert_MarkupText_to_HTML.py
@@ -0,0 +1,19 @@
+import re
+
+def rewrite_text_file(input_file, output_file):
+    with open(input_file, 'r') as file:
+        content = file.read()
+
+    # Replace [text](link) with <a href="link">text</a>
+    modified_content = re.sub(r'\[(.*?)\]\((.*?)\)', r'<a href="\2">\1</a>', content)
+
+    with open(output_file, 'w') as file:
+        file.write(modified_content)
+
+    print("File rewritten successfully!")
+
+# Usage example
+input_file_path = 'input.txt'  # Provide the path to your input file here
+output_file_path = 'output.txt'  # Provide the path to the desired output file here
+
+rewrite_text_file(input_file_path, output_file_path)
